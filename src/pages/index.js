3a4b5f0e27b2b21bcc07eb85679582d22aa21e39
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-02-21 16:54:31 +0600

    update grammar

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,35 @@ export default function Home() {
       <Head title="Home" />
       <div className={container}>
         <h1>Hi, I am Shafi Rayhan!</h1>
-        <p>I'm an experienced Web Developer from Sylhet, Bangladesh. <br />
-          I was introduced to programming in 2019 but had to let go because of my study.
-          But then COVID-19 came and I decided to try it again, and I've been trying ever since.
+        <p>
+          I'm an experienced Web Developer from Sylhet, Bangladesh.
+          {' '}
+          <br />
+          I was
+          introduced to programming in 2019 but had to let go because of my
+          study. But then COVID-19 came and I decided to try it again, and I've
+          been trying ever since.
         </p>
         <p>
-          I started building websites with the core web technologies like HTML, CSS, JavaScript.
-          Later on, I learned React and Gatsby and built some cool stuff. <br /> After a while, I decided to apply for a job as a Web Developer and to my surprise, I got a job at <a href="https://www.arrowsoft.co/" target="_blank" rel="noreferrer">Arrowsoft</a>.
-          There I got to learn some more awesome technologies including but not limited to Angular, NodeJS, Ionic, etc.
+          I started building websites with the core web technologies like HTML,
+          CSS and JavaScript. Later on, I learned React and Gatsby and built
+          some cool stuff.
+          {' '}
+          <br />
+          {' '}
+          After a while, I decided to apply for a job as
+          a Web Developer and to my surprise, I got a job at
+          {' '}
+          <a href="https://www.arrowsoft.co/" target="_blank" rel="noreferrer">
+            Arrowsoft
+          </a>
+          . There I got to learn some more awesome technologies including but
+          not limited to Angular, NodeJS, Ionic, etc.
         </p>
-        <p>I recently graduated as a Power Engineer and Automobile Engineer!</p>
-        <a className={anchor} href="mailto:rayhanshafi7@gmail.com">Contact Me</a>
+        <p>I recently graduated as a Power and Automobile Engineer!</p>
+        <a className={anchor} href="mailto:rayhanshafi7@gmail.com">
+          Contact Me
+        </a>
       </div>
     </Layout>
   );
