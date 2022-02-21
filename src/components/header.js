0fa96bc35208b551d5c80960cba9d96413bd0992
Shafi Rayhan <rayhanshafi7@gmail.com>
2022-02-21 13:45:23 +0600

    show cms link in header

--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,31 @@
 import { Link } from 'gatsby';
 import React from 'react';
+import create from '../assets/create.svg';
 import { active, header } from '../styles/components/header.module.css';
 
 export default function Header() {
   return (
     <header className={header}>
-        <Link to="/">
-          <h1>Shafi</h1>
+      <Link to="/">
+        <h1>Shafi</h1>
+      </Link>
+      <nav>
+        <Link activeClassName={active} to="/">
+          Home
         </Link>
-        <nav>
-          <Link activeClassName={active} to="/">Home</Link>
-          <Link
-            to="/blog"
-            activeClassName={active}
-            partiallyActive
-          >
-            Blog
-          </Link>
-          <Link activeClassName={active} to="/works">Works</Link>
-          <Link activeClassName={active} to="/contact">Contact</Link>
-        </nav>
+        <Link to="/blog" activeClassName={active} partiallyActive>
+          Blog
+        </Link>
+        <Link activeClassName={active} to="/works">
+          Works
+        </Link>
+        <Link activeClassName={active} to="/contact">
+          Contact
+        </Link>
+        <Link to="/admin/" title="Create New Blog Post">
+          <img src={create} alt="" />
+        </Link>
+      </nav>
     </header>
   );
 }
